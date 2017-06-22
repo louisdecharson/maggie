a01677b14de87e54489309c694f82c88842a65bb
LouisdeCharson <ltdc2121@hotmail.fr>
2017-06-22 19:08:17 +0200

    improve css

--- public/js/maggie.js
+++ public/js/maggie.js
@@ -1,35 +1,18 @@
-function fontResize(elem,factor) {
-    // var fontsize = (parseFloat(elem.css('font-size'))*factor).toString()+'px';
-    // elem.css('font-size',fontsize);
-}
-
-$(document).ready(function(){
- 
-    var sum=0;
-    $('#tudobem-menu li').each( function(){ sum += $(this).width(); console.log($(this).width()); });
-    $('#tudobem-menu ul').css('width');
-    $('.tudobem-center').each(function(index) {
-        var w = -$(this).outerWidth(true)/2;;
-        $(this).css({'margin-left': w });
-    });
-
+$(document).ready(function(){ 
     $(window).resize(function() {
-        var sum=0;
-        $('#tudobem-menu li').each( function(){ sum += $(this).width(); console.log($(this).width()); });
-        $('#tudobem-menu ul').css('width');
         $('.tudobem-center').each(function(index) {
             var w = -$(this).width()/2;;
             $(this).css({'margin-left': w });
         });
-        
     }).resize();
 
     $('#tudobem-click-about').click(function(){
         if ($(this).hasClass('hidden')) {
             $('#tudobem-about').show();
             $('#tudobem-dates').hide();
-            $('#tudobem-about').width(sum);
-            $('#tudobem-about').css({'margin-left': -(1.15*sum)/2 });
+            var widthTitle = $('#tudobem-title').width();
+            $('#tudobem-about').width(widthTitle);
+            $('#tudobem-about').css({'margin-left': -(widthTitle)/2 });
             $(this).removeClass('hidden');
             $(this).addClass('shown');
             $('#tudobem-click-dates').addClass('hidden');
@@ -44,8 +27,10 @@ $(document).ready(function(){
         if ($(this).hasClass('hidden')) {
             $('#tudobem-dates').show();
             $('#tudobem-about').hide();
-            $('#tudobem-dates').width(sum);
-            $('#tudobem-dates').css({'margin-left': -1.15*sum/2 });
+            var widthTitle = $('#tudobem-title').width();
+            $('#tudobem-dates').width(widthTitle);
+            console.log(widthTitle);
+            $('#tudobem-dates').css({'margin-left': -widthTitle/2 });
             $(this).removeClass('hidden');
             $(this).addClass('shown');
             $('#tudobem-click-about').addClass('hidden');
